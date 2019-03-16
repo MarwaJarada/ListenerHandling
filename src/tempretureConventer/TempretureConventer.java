8c6bfca92ed8a88aaaa8220222275524aa5a8e5c
Your-Full-Name <jaradamarwa@gmail.com>
2019-03-16 18:55:29 +0200

    define basic component

--- src/tempretureConventer/TempretureConventer.java
+++ src/tempretureConventer/TempretureConventer.java
@@ -1,10 +1,22 @@
 package tempretureConventer;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TempretureConventer extends JFrame {
+    JLabel celsiusLabel;
+    JTextField celsuisTextfield;
+    ButtonGroup buttonGroup;
+    JRadioButton fahrenheitRadioBtn;
+    JRadioButton kelvinRadioBtn;
+    JLabel resultTempraturer;
 
-
-
-
+    public TempretureConventer(){
+        celsiusLabel=new JLabel();
+        celsuisTextfield= new JTextField();
+        buttonGroup=new ButtonGroup();
+        fahrenheitRadioBtn=new JRadioButton();
+        kelvinRadioBtn=new JRadioButton();
+        resultTempraturer=new JLabel();
+    }
 }
