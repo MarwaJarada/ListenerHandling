7ec80dd80279f7ab6db806feae1ebf61d9829296
Your-Full-Name <jaradamarwa@gmail.com>
2019-03-16 19:05:57 +0200

    preparing basic GUI properities

--- src/tempretureConventer/TempretureConventer.java
+++ src/tempretureConventer/TempretureConventer.java
@@ -1,9 +1,14 @@
+
 package tempretureConventer;
+/**
+ * @author Marwa N. Jarada
+ */
 
 import javax.swing.*;
 import java.awt.*;
 
 public class TempretureConventer extends JFrame {
+    // Components :
     JLabel celsiusLabel;
     JTextField celsuisTextfield;
     ButtonGroup buttonGroup;
@@ -18,5 +23,24 @@ public class TempretureConventer extends JFrame {
         fahrenheitRadioBtn=new JRadioButton();
         kelvinRadioBtn=new JRadioButton();
         resultTempraturer=new JLabel();
+
+        //Basic properities
+
+        setLayout(new GridLayout(4,1));//4row,1col
+        setTitle("Temperature conventer");
+        setResizable(true);
+        setLocationRelativeTo(null); // To show the output in the middle of screen
+        setSize(500,250);
+
+
+
+
+        setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) {
+        TempretureConventer tempretureConventer=new TempretureConventer();
     }
 }
