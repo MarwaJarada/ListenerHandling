e548a8af5d5ee178bf24f1c14ebf21768e1f1737
Your-Full-Name <jaradamarwa@gmail.com>
2019-03-17 11:23:50 +0200

    basic component of GUI

--- src/GussingGame.java
+++ src/GussingGame.java
@@ -1,10 +1,30 @@
-public class GussingGame {
+import javax.swing.*;
 
-    
+public class GussingGame extends JFrame{
 
+    JLabel rangeNumLabel;
+    JLabel guessLabel;
+    JLabel resultNum;
+    JTextField numTxtField;
+    JButton newGameBtn;
 
     public GussingGame(){
+        setTitle("Gussing Game");
+        setLocationRelativeTo(null);
+        setSize(300,150);
 
+        rangeNumLabel=new JLabel();
+        guessLabel=new JLabel();
+        resultNum=new JLabel();
+
+        numTxtField=new JTextField();
+        newGameBtn=new JButton();
+        
+
+
+
+
+        setVisible(true);
 
     }
 
