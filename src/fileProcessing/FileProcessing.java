8ec8bac27bc232cda8eee99e0b816e28cb5b431a
Your-Full-Name <jaradamarwa@gmail.com>
2019-03-23 19:16:41 +0200

    done

--- src/fileProcessing/FileProcessing.java
+++ src/fileProcessing/FileProcessing.java
@@ -2,10 +2,7 @@ package fileProcessing;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -50,6 +47,12 @@ public class FileProcessing extends JFrame{
         font.addActionListener(actionListener);
         color=new JMenuItem("Color");
         color.addActionListener(actionListener);
+
+
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+        close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK));
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_MASK));
+        font.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_MASK));
 
 
         fileMenu.add(open);
@@ -98,7 +101,10 @@ public class FileProcessing extends JFrame{
                     break;
                 case "color":
 
-
+                    colorChooser = JColorChooser.showDialog(FileProcessing.this,
+                            "Choose Font color :",
+                            Color.YELLOW);
+                    fileTxt.setForeground(colorChooser);
                     break;
             }
         }
@@ -112,22 +118,25 @@ public class FileProcessing extends JFrame{
                 15);
         fileTxt.setFont(new Font("Tahoma",0,fontSize));
 
-}
+    }
 
-    public void  openFileChooser(){
+    public void  openFileChooser() {
         fileChooser = new JFileChooser();
-        fileChooser.showDialog(FileProcessing.this,"Select");
-        String selectedFile = fileChooser.getSelectedFile().getName();
-        String content = selectedFile;
-        fileTxt.setText(content);
-
+        fileChooser.showDialog(FileProcessing.this, "Select");
         try {
-            if (fileTxt.getText()!=null){
+            File selectedFile = fileChooser.getSelectedFile();
             Scanner scanner = new Scanner(selectedFile);
-            fileTxt.setText(scanner.toString());
-            fileTxt.setEditable(true);}
-        } catch (Exception e) {
-            e.printStackTrace();
+            while (scanner.hasNext()){
+                fileTxt.append(scanner.nextLine());
+
+
+            }
+            fileTxt.setEditable(true);
+        }catch (Exception e) {
+            System.out.println("Empty File");
         }
+
+
+
     }
 }
--- src/multipleSelection/MultipleSelection.java
+++ src/multipleSelection/MultipleSelection.java
@@ -1,46 +1,60 @@
+
 package multipleSelection;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MultipleSelection extends JFrame{
-    JButton copyBtn;
-    JTextArea txtArea;
-    JList<String> list;
+        JButton copyBtn;
+        JTextArea txtArea;
+        JList<String> list;
 
 
 
-public static void main(String[] args){
-        MultipleSelection list=new MultipleSelection();
-        list.setMultipleSelectionListJFrame();
-
-        }
-
-
-public void setMultipleSelectionListJFrame(){
-
-        this.setTitle("Multiple Selection Lists");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
-        this.setSize(400,200);
-        this.setLayout(new FlowLayout());
-        String[] myList={"Black","Blue","Cyan","Dark Grary","Gray","Blasck","Blue","Cyan","Dark Grary","Gray"};
-        list=new JList<>(myList);
-        list.setSelectedIndex(0);
-        list.setPreferredSize(new Dimension(60,100));
-        JScrollPane scrollPane=new JScrollPane(list);
-        this.getContentPane().add(scrollPane);
-        copyBtn=new JButton("Copy >>");
-        txtArea=new JTextArea("Black\nCyan\nGray");
-        txtArea.setPreferredSize(new Dimension(100,100));
-
-        this.add(list);
-        this.add(copyBtn);
-        this.add(txtArea);
-
-        this.setVisible(true);
-
+        public static void main(String[] args){
+                MultipleSelection list=new MultipleSelection();
+                list.setMultipleSelectionListJFrame();
 
         }
 
+
+        public void setMultipleSelectionListJFrame(){
+
+                this.setTitle("Multiple Selection Lists");
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                this.setResizable(true);
+                this.setSize(400,200);
+                this.setLayout(new FlowLayout());
+                String[] myList={"Black","Blue","Cyan","Dark Grary","Gray"};
+                list=new JList<>(myList);
+                list.setPreferredSize(new Dimension(60,100));
+                list.setVisibleRowCount(5);
+                JScrollPane scrollPane=new JScrollPane(list);
+
+                copyBtn=new JButton("Copy >>");
+                copyBtn.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent actionEvent) {
+                                if (list.isSelectionEmpty()){
+                                        JOptionPane.showMessageDialog(MultipleSelection.this,"No items Selected !");
+                                }
+                                else {
+                                        for (int x=0 ; x<list.getSelectedValuesList().size() ;x++){
+                                                txtArea.append(list.getSelectedValuesList().get(x)+"\n");}
+                                }
+                        }
+                });
+                txtArea=new JTextArea();
+                txtArea.setPreferredSize(new Dimension(100,100));
+                this.add(list);
+                this.add(scrollPane);
+                this.add(copyBtn);
+                this.add(txtArea);
+                this.setVisible(true);
+
+
         }
+
+}
